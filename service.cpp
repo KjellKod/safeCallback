db6a7ad0e1ffd755426e262ed346ef8fdf7b8ef6
Fei <fei.yan@nokia.com>
2016-05-16 18:25:23 +0800

    fix newly found race conditon and wrapper sync variables into the pointer itself

--- service.cpp
+++ service.cpp
@@ -5,12 +5,17 @@ using namespace std;
 
 std::vector<int> Service::probeMarker{};
 
+struct Service::SyncHolder{
+    std::mutex mutex;
+    bool       released {false};
+};
+
 Service::Service(Caller& c) : caller_(c){
     probeMarker.clear(); //only track one instance actually
 
-    sync_ = make_shared<int>(0);
+    sync_ = make_shared<SyncHolder>();
     resource_ = make_unique<int>(0);
-    std::weak_ptr<int> ref(sync_);
+    std::weak_ptr<SyncHolder> ref(sync_);
     caller_.registerFor(0, [this, ref = std::move(ref)](){
             doSomething(ref);
             });
@@ -19,20 +24,31 @@ Service::Service(Caller& c) : caller_(c){
 Service::~Service(){
     probeMarker.push_back(0);
     caller_.unregister(0); 
+
     probeMarker.push_back(1);
+    std::weak_ptr<SyncHolder> ref(sync_);
     sync_ = nullptr;
     probeMarker.push_back(2);
 
-    lock_guard<mutex> lock(mutex_);
-    resource_ = nullptr;
+    if (!ref.expired()){
+        auto sync = ref.lock();
+        lock_guard<mutex> lock(sync->mutex);
+        resource_ = nullptr;
+        sync->released = true;
+    }else{
+        resource_ = nullptr;
+    }
     probeMarker.push_back(3);
 }
 
-void Service::doSomething(std::weak_ptr<int> ref){
+void Service::doSomething(std::weak_ptr<SyncHolder> ref){
     probeMarker.push_back(11);
     if (auto sync = ref.lock()){
-        lock_guard<mutex> lock(mutex_);
-        *resource_ = 123;
+        lock_guard<mutex> lock(sync->mutex);
+        if (!(sync->released)){
+            //possibly dtor obtain mutex first
+            *resource_ = 123;
+        }
         probeMarker.push_back(12);
     }
 }
--- service.h
+++ service.h
@@ -5,18 +5,21 @@
 
 class Caller;
 class Service{
+    private:
+        struct SyncHolder;
+
     public:
         Service(Caller& caller);
         Service& operator=(const Service&) = delete;
         ~Service();
         
-        void doSomething(std::weak_ptr<int> ref);
+        void doSomething(std::weak_ptr<SyncHolder> ref);
 
         //testing purpose only
         static std::vector<int> probeMarker;
+
     private:
-        std::mutex mutex_;
-        std::shared_ptr<int> sync_;
+        std::shared_ptr<SyncHolder> sync_;
         std::unique_ptr<int> resource_;
 
         Caller& caller_;
